64642b02d031897f3fc0e3ec92fa18caa7045934
bbones <boneswilliam@gmail.com>
2018-02-06 17:28:11 +0200

    Setup

---
+++ test/test.js
@@ -0,0 +1,13 @@
+/* global describe it */
+var expect = require('chai').expect
+var muting = require('../muting')
+
+describe('muting', function () {
+  describe('init()', function () {
+    it('should inject an object into twitter', function () {
+      var twitter = 'twitter'
+      muting.init(twitter)
+      expect(muting.twitter).to.equal(twitter)
+    })
+  })
+})
