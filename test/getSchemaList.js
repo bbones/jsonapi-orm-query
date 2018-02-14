49b20578e0495678168cf58e82fc2eccf46c2e00
bbones <boneswilliam@gmail.com>
2018-02-14 17:57:12 +0200

    async/await test db query fixed

--- test/getSchemaList.js
+++ test/getSchemaList.js
@@ -1,25 +1,34 @@
-/* global describe it, before */
+/* global describe it, before, after */
 var expect = require('chai').expect
 const { Client } = require('pg')
 
 describe('SchemaList check', () => {
   let client
   before(async () => {
-    console.log(process.env)
     client = new Client(process.env.DATABASE_URL)
     await client.connect()
   })
-  it('Schema list', async (done) => {
-    let res
-    try {
-      res = await client.query('SELECT $1::text as message', ['Hello world!'])
-      console.log(res)
-      await expect(res.rows[0].message).to.equal('Hello world!')
-      await client.end()
-      done()
 
-    } catch (e) {
-      done(e)
-    }
+  it('Connect', async () => {
+    let res = await client.query('SELECT $1::text as message', ['Hello world!'])
+    expect(res.rows[0].message).to.equal('Hello world!')
+  })
+
+  it('Schemas query', async () => {
+    let res = await client.query('select schema_name from information_schema.schemata')
+    expect(res.rows.find((element) => {
+      return element.schema_name === 'keeper'
+    }).schema_name).to.equal('keeper')
+  })
+
+  it('Table query', async () => {
+    let res = await client.query('SELECT table_name FROM information_schema.tables')
+    expect(res.rows.find((element) => {
+      return element.table_name === 'object'
+    }).table_name).to.equal('object')
+  })
+
+  after(async () => {
+    await client.end()
   })
 })
